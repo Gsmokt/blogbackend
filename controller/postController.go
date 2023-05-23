1528971a72c485a3f8618cebadd88f33d467d016
Grzegorz Smoktunowicz <biarth@wp.pl>
2023-05-23 23:16:13 +0200

    add get, post, update controller

--- controller/postController.go
+++ controller/postController.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 
 	"github.com/Gsmokt/blogbackend/database"
 	"github.com/Gsmokt/blogbackend/models"
@@ -21,5 +23,49 @@ func CreatePost(c *fiber.Ctx) error {
 	}
 	return c.JSON(fiber.Map{
 		"message": "Congratulation!, Your post is live",
+	})
+}
+
+func AllPost(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	limit := 5
+	offset := (page - 1) * limit
+	var total int64
+	var getblog []models.Blog
+	database.DB.Preload("User").Offset(offset).Limit(limit).Find(&getblog)
+	database.DB.Model(&models.Blog{}).Count(&total)
+
+	return c.JSON(fiber.Map{
+		"data": getblog,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      page,
+			"last_page": math.Ceil(float64(int(total) / limit)),
+		},
+	})
+}
+
+func DetailPost(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	var blogpost models.Blog
+	database.DB.Where("id=?", id).Preload("User").First(&blogpost)
+	return c.JSON(fiber.Map{
+		"data": blogpost,
+	})
+}
+
+func UpdatePost(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+	blog := models.Blog{
+		Id: uint(id),
+	}
+	var blogpost models.Blog
+	if err := c.BodyParser(&blogpost); err != nil {
+		fmt.Println("Unable to parse body")
+	}
+	database.DB.Model(&blog).Updates(blog)
+
+	return c.JSON(fiber.Map{
+		"message": "post updated successfully",
 	})
 }
